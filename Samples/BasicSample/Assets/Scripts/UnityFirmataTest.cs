e84ef5e475f0aaecc6bac73f1291dc709e73170d
xiaowp <xiaowp@gmail.com>
2014-11-30 12:44:06 +0800

    Add basic sample.
    Light on LED seems work but not always.

---
+++ Samples/BasicSample/Assets/Scripts/UnityFirmataTest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Sharpduino;
+using Sharpduino.Constants;
+
+public class UnityFirmataTest : MonoBehaviour {
+
+	private ArduinoUno arduino;
+	private bool ledOn = false;
+
+	// Use this for initialization
+	void Start () {
+		arduino = new ArduinoUno("/dev/tty.usbmodemfd121");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+				
+    void OnGUI() {
+		// LED Update
+		var ledRect = new Rect(10, 50, 50, 50);
+		var tmp = GUI.Toggle(ledRect, ledOn, "LED");
+		if(ledOn != tmp) {
+			Debug.Log(tmp);
+			ledOn = tmp;
+			arduino.SetPinMode(ArduinoUnoPins.D13,PinModes.Output);
+			arduino.SetDO (ArduinoUnoPins.D13, ledOn);
+		}
+    }
+	
+	void OnApplicationQuit() 
+    {
+		if(arduino != null) {
+			arduino.Dispose ();
+		}
+    }
+}
